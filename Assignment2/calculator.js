90bdd071cd35c6861b86cbaa5626d2d47add0e72
Calvin Lau <lau.calvin@live.com>
2019-02-05 17:45:19 -0700

    2-operand calculations done

--- Assignment2/calculator.js
+++ Assignment2/calculator.js
@@ -1,3 +1,5 @@
+let computeFlag = false;
+
 window.addEventListener("load", function() {
     initializeBtnListeners();
 });
@@ -13,6 +15,7 @@ function initializeBtnListeners() {
 
 function processClick(input) {
     let resultElement = document.getElementById("result");
+    let historyElement = document.getElementById("history");
     switch (input) {
         case "CE":
             if (resultElement.value[resultElement.value.length - 1] === " ") {
@@ -21,7 +24,7 @@ function processClick(input) {
             } else if (resultElement.value === "0"){
                 // Ignore if input equals 0
                 break;
-            } else if (resultElement.value.length === 1) {
+            } else if (resultElement.value.length === 1 || resultElement.value === "Infinity") {
                 // Change input to 0 when deleting last character that is not zero
                 resultElement.value = "0";
             } else {
@@ -30,7 +33,7 @@ function processClick(input) {
             }
             break;
         case "=":
-            calculate();
+            processInput();
             break;
         case ".":
             if (resultElement.value.indexOf(".") !== -1) {
@@ -60,9 +63,17 @@ function processClick(input) {
                 resultElement.value += " " + input + " ";
             }
             break;
+        case "(":
+        case ")":
+            resultElement.value += " " + input + " ";
+            break;
         default:
             if (resultElement.value === "0") {
                 resultElement.value = input;
+            } else if (historyElement.value !== " " && Number(resultElement.value) > 0 &&
+                        computeFlag === true) {
+                resultElement.value = input;
+                computeFlag = false;
             } else {
                 resultElement.value += input;
             }
@@ -70,14 +81,52 @@ function processClick(input) {
     }
 }
 
-function calculate() {
+function processInput() {
     let resultElement = document.getElementById("result");
     let historyElement = document.getElementById("history");
 
 
-    let inputArray = resultElement.value.split(" ");
-    console.log (inputArray);
+    // let processedArray = calculate(resultElement.value);
+    let val = calculate(resultElement.value);
 
-    historyElement.innerText = resultElement.value;
-    resultElement.value = "0";
+    if (val === false) {
+        historyElement.innerText = resultElement.value + " =ERROR";
+        return;
+    } else {
+        computeFlag = true;
+        historyElement.innerText = resultElement.value + " =";
+        resultElement.value = val.toString();
+    }
+}
+
+function calculate(inputString) {
+    let inputArray = inputString.split(" ");
+
+    if (inputArray.length === 1) {
+        return inputArray[0];
+    }
+
+    let result = 0;
+    for (let i=0; i<inputArray.length; i++) {
+        if (isNaN(result)) {
+            return false;
+        }
+
+        switch (inputArray[i]) {
+            case "/":
+                result = Number(inputArray[i-1]) / Number(inputArray[i+1]);
+                break;
+            case "x":
+                result = Number(inputArray[i-1]) * Number(inputArray[i+1]);
+                break;
+            case "-":
+                result = Number(inputArray[i-1]) - Number(inputArray[i+1]);
+                break;
+            case "+":
+                result = Number(inputArray[i-1]) + Number(inputArray[i+1]);
+                break;
+        }
+    }
+
+    return result;
 }
