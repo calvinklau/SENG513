554e2a7f194746ceccc4b98446abea95dcc0e494
Calvin Lau <lau.calvin@live.com>
2019-02-15 16:00:40 -0700

    simplified input processing, assignment complete

--- Assignment2/calculator.js
+++ Assignment2/calculator.js
@@ -1,157 +1,79 @@
-let computeFlag = false;
+// Wrapper function to avoid polluting global variable namespace
+(function() {
+    let computeFlag = false;
 
-window.addEventListener("load", function() {
-    initializeBtnListeners();
-});
+    window.addEventListener("load", function() {
+        initializeBtnListeners();
+    });
 
-function initializeBtnListeners() {
-    let buttons = document.getElementsByTagName("button");
-    for (let btn of buttons) {
-        btn.onclick = function() {
-            processClick(btn.innerText);
+    function initializeBtnListeners() {
+        let buttons = document.getElementsByTagName("button");
+        for (let btn of buttons) {
+            // Call processInput() on button click
+            btn.onclick = function() {
+                processInput(btn.innerText);
+            }
         }
     }
-}
 
-function processClick(input) {
-    let resultElement = document.getElementById("result");
-    let historyElement = document.getElementById("history");
-    switch (input) {
-        case "CE":
-            if (resultElement.value[resultElement.value.length - 1] === " ") {
-                // Delete operator and associated space chars
-                resultElement.value = resultElement.value.substring(0, resultElement.value.length - 3);
-            } else if (resultElement.value === "0"){
-                // Ignore if input equals 0
-                break;
-            } else if (resultElement.value.length === 1 || resultElement.value === "Infinity") {
-                // Change input to 0 when deleting last character that is not zero
-                resultElement.value = "0";
-            } else {
-                // Delete rightmost character
-                resultElement.value = resultElement.value.substring(0, resultElement.value.length - 1);
-            }
-            break;
-        case "=":
-            processInput();
-            break;
-        case ".":
-            if (resultElement.value.indexOf(".") !== -1) {
-                break;
-            } else {
-                resultElement.value += input;
-            }
-            break;
-        case "0":
-            if (resultElement.value === "0") {
-                break;
-            } else if (historyElement.value !== " " &&
-                Number(resultElement.value) > 0 &&
-                computeFlag === true) {
-                resultElement.value = input;
-                computeFlag = false;
-            } else {
-                resultElement.value += input;
-            }
-            break;
-        case "/":
-        case "x":
-        case "-":
-        case "+":
-            if (resultElement.value[resultElement.value.length - 1] === " ") {
-                if (resultElement.value[resultElement.value.length - 2] === input) {
-                    break;
+    function processInput(input) {
+        let resultElement = document.getElementById("result");
+        let historyElement = document.getElementById("history");
+        let clearBtnText = document.getElementById("clear");
+        switch (input) {
+            case "CE":
+                if (resultElement.value[resultElement.value.length - 1] === " ") {
+                    // Delete operator and associated space chars
+                    resultElement.value = resultElement.value.substring(0, resultElement.value.length - 3);
+                } else if (resultElement.value.length === 1 || resultElement.value === "0") {
+                    // Change input field to 0 if deleting single non-zero digit or if deleting zero
+                    resultElement.value = "0";
                 } else {
-                    resultElement.value = resultElement.value.substring(0, resultElement.value.length - 2) + input + " ";
+                    // Delete rightmost character
+                    resultElement.value = resultElement.value.substring(0, resultElement.value.length - 1);
                 }
-            } else {
-                resultElement.value += " " + input + " ";
-            }
-            break;
-        case "(":
-        case ")":
-            if (resultElement.value === "0") {
-                resultElement.value = input;
-            } else if (historyElement.value !== " " &&
-                Number(resultElement.value) > 0 &&
-                computeFlag === true) {
-                resultElement.value = input;
-                computeFlag = false;
-            } else {
-                resultElement.value += " " + input + " ";
-            }
-            break;
-        default:
-            if (resultElement.value === "0") {
-                resultElement.value = input;
-            } else if (historyElement.value !== " " &&
-                        Number(resultElement.value) > 0 &&
-                        computeFlag === true) {
-                // Replace previous answer with new digit
-                resultElement.value = input;
-                computeFlag = false;
-            } else {
-                resultElement.value += input;
-            }
-            break;
-    }
-}
-
-function processInput() {
-    let resultElement = document.getElementById("result");
-    let historyElement = document.getElementById("history");
-
-
-    // let processedArray = calculate(resultElement.value);
-    let val = calculate(resultElement.value);
-
-    if (val === false) {
-        historyElement.innerText = resultElement.value + " =ERROR";
-
-        return;
-    } else {
-        computeFlag = true;
-        historyElement.innerText = resultElement.value + " =";
-        resultElement.value = val.toString();
-    }
-}
-
-function calculate(inputString) {
-    let inputArray = inputString.split(" ");
-
-    if (inputArray.length === 1) {
-        if (isNaN(inputArray[0])) {
-            return false;
-        } else {
-            return inputArray[0];
-        }
-    }
-
-    let result = 0;
-    for (let i=0; i<inputArray.length; i++) {
-        if (isNaN(result)) {
-            return false;
-        }
-
-        switch (inputArray[i]) {
+                break;
+            case "AC":
+                // Clear entire input field
+                resultElement.value = "0";
+                clearBtnText.innerText = "CE";
+                break;
+            case "=":
+                // Calculate inputted expression
+                calculate(resultElement, historyElement);
+                clearBtnText.innerText = "AC";
+                break;
             case "/":
-                result = Number(inputArray[i-1]) / Number(inputArray[i+1]);
-                inputArray = inputArray.splice(inputArray[i-1], 3, result);
-                break;
             case "x":
-                result = Number(inputArray[i-1]) * Number(inputArray[i+1]);
-                inputArray = inputArray.splice(inputArray[i-1], 3, Number(result));
-                break;
             case "-":
-                result = Number(inputArray[i-1]) - Number(inputArray[i+1]);
-                inputArray = inputArray.splice(inputArray[i-1], 3, result);
-                break;
             case "+":
-                result = Number(inputArray[i-1]) + Number(inputArray[i+1]);
-                inputArray = inputArray.splice(inputArray[i-1], 3, result);
+                // Pad math operators with spaces for readability
+                resultElement.value += " " + input + " ";
+                break;
+            default:
+                // Append digits or periods
+                if (resultElement.value === "0") {
+                    resultElement.value = input;
+                } else {
+                    resultElement.value += input;
+                }
                 break;
         }
     }
 
-    return result;
-}
+    // Evaluate inputted expression
+    function calculate(resultEl, historyEl) {
+        let expression = resultEl.value;
+        let result = 0;
+        try {
+            result = eval(expression.replace("x", "*"));
+            resultEl.value = result.toString();
+            historyEl.innerText = expression + " = ";
+        } catch (err) {
+            // Display error
+            console.log(expression.replace("x", "*"));
+            resultEl.value = expression;
+            historyEl.innerText = expression + " = ERROR";
+        }
+    }
+})();
